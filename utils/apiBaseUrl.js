18c807f6255ffd46863f2acee1b3361acbf4ab15
inzimamulhaq <inzemamsmile2@gmail.com>
2022-11-15 13:04:36 +0500

    updated url v2

--- utils/apiBaseUrl.js
+++ utils/apiBaseUrl.js
@@ -3,7 +3,7 @@ let apiBaseUrl
 if (process.env.NODE_ENV === "development") {
   apiBaseUrl = process.env.DEV_API_URL
 } else {
-  apiBaseUrl = process.env.PRODUCTION_API_URL
+  apiBaseUrl = process.env.DEV_API_URL
 }
 
 export default apiBaseUrl
